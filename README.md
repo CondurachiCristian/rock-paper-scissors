94c40e20e5a7db15bb323cdbf5c9e4f6bd17b529
CondurachiCristian <107665724+CondurachiCristian@users.noreply.github.com>
2025-08-10 11:04:16 +0300

    Add description of project, link index.html and app.js and add PSEUDOCODE

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This will be a simple rock paper scissors project.
---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+    PROMPT user for "rock" , "paper" or "scissors"
+    CALCULATE computer choice
+    IF user choice beats computer choice THEN INCREMENT user score by 1
+    ELSE INCREMENT computer score by 1 
+    REPEAT sequence until score of 5 is reached by either user or computer
+*/
+
